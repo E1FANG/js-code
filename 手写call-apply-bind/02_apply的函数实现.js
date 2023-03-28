9fa6499b8875c377a51a65234a1c234ed10dafcd
e1fang <438283787@qq.com>
2023-03-28 18:44:18 +0800

    feat: 手写apply

---
+++ 手写call-apply-bind/02_apply的函数实现.js
@@ -0,0 +1,28 @@
+Function.prototype.myApply = function (thisArg,argArray){
+  // 获取要调用的函数
+  var fn = this
+
+  // 非对象this指向window
+  thisArg = thisArg ? Object(thisArg) : window
+
+  //  绑定this，执行
+  thisArg.fn = fn
+  var arg = argArray || []
+  var result = thisArg.fn(...arg)
+  delete thisArg.fn
+
+  return result
+}
+
+function sum(num1,num2){
+  return num1 + num2
+}
+
+// 系统调用
+var res = sum.apply('abc',[1,2])
+console.log('系统调用',res)
+
+// myApply
+
+var res2 = sum.myApply('abc',[3,4])
+console.log('myApply',res2)
