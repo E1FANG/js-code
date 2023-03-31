315ee353635cb9b274edfc61a6d89fdbde3bb2d2
e1fang <438283787@qq.com>
2023-03-31 20:13:05 +0800

    feat: compose函数接收多个参数

--- 函数的柯里化/07_手写compose函数.js
+++ 函数的柯里化/07_手写compose函数.js
@@ -20,13 +20,15 @@ function composeFn(...fns){
 
 var newFn = composeFn(double,square)
 
-console.log(newFn(10))
+// console.log(newFn(10))
 
 // 之后发现，其实初始值就是传给函数的参数
 // 传给函数的参数就是给第一个函数使用的，之后的函数都是使用之前函数的结果
 function composeFn2(...fns){
   return function(...args){
-    return fns.reduce((p,c)=>c(p),...args)
+    return fns.reduce((p,c)=>{ 
+      return Array.isArray(p) ? c(...p) : c(p)
+    },args)
   }
 }
 
@@ -34,6 +36,15 @@ function sum(n,m){
   return n + m 
 }
 
-var newFn2 = composeFn(sum,double,square)
+var newFn2 = composeFn2(sum,double,square)
 
 console.log(newFn2(5,5))
+
+// 如果我的函数链中需要多个参数,就需要在解构了（composeFn2内部）
+function getTwoNumber(){
+  return [5,5]
+}
+
+var newFn3 = composeFn2(getTwoNumber,sum,double,square)
+
+console.log( newFn3())
